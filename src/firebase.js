1b2b7383f2cf912252368c319b83452fd9eeb7e2
Jasmin Li <jl6ww@virginia.edu>
2019-05-28 15:23:48 -0400

    Edited firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBSv_p6PjWDWkgTFtmxGxCWEXS7RcRlzmE",
-  authDomain: "fir-daily-challenge.firebaseapp.com",
-  databaseURL: "https://fir-daily-challenge.firebaseio.com",
-  projectId: "fir-daily-challenge",
-  storageBucket: "fir-daily-challenge.appspot.com",
-  messagingSenderId: "469745708986",
-  appId: "1:469745708986:web:09e92f08a56ab527"
+  apiKey: "AIzaSyBaSNO98KKS4Dz7MpeZlMA9JqrBw8s6I6w",
+  authDomain: "pomodoro-212c5.firebaseapp.com",
+  databaseURL: "https://pomodoro-212c5.firebaseio.com",
+  projectId: "pomodoro-212c5",
+  storageBucket: "pomodoro-212c5.appspot.com",
+  messagingSenderId: "703957610674",
+  appId: "1:703957610674:web:dc61f0f811c01f0f"
 };
 
 firebase.initializeApp(firebaseConfig);
