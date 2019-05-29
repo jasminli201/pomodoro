f4b89ae5ea563966a2884e69ddc53ceba482bfab
John Ramirez <jvramirez13@gmail.com>
2019-05-29 10:54:11 -0400

    Updated Form.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Timer from "./Timer.js";
+import Welcome from "./Welcome.js";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <Welcome />
       <Timer />
     </div>
   );
