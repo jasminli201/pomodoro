f7ff0c70403acedc647062ec3c5c7198af678855
Jasmin Li <jl6ww@virginia.edu>
2019-05-29 09:28:15 -0400

    added navbar and updated UI

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { Component } from "react";
+import { Link, Route, BrowserRouter as Router } from "react-router-dom";
 import Timer from "./Timer.js";
+// import History from "./History.js";
+// import Profile from "./Profile.js";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <Timer />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        {/* <Route exact path="/" component={Home} /> */}
+        <Route exact path="/Timer" component={Timer} />
+        {/* <Route exact path="/History" component={History} /> */}
+        {/* <Route exact path="/Profile" component={Profile} /> */}
+      </Router>
+    );
+  }
 }
 
 export default App;
